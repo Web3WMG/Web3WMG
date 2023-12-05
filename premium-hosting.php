980e8999ca1975e73221a22faaf45e97fa63c656
The Homogeneral <146903198+homogeneral@users.noreply.github.com>
2023-12-05 17:59:57 -0600

    3WMG

---
+++ premium-hosting.php
@@ -0,0 +1,332 @@
+<?
+// This is used to constuct the cPanel login ur>ol
+include('geturl.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Template designed by iFastNet (iFastNet.com) exclusively for MyOwnFreeHost.com users -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="initial-scale=1 width=device-width">
+	<title>Premium Hosting</title>
+	<meta name="description" content="Professional Free Hosting">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<!-- The Famous html Shiv or Shim -->
+	<!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+    <![endif]-->
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<nav class="navbar navbar-default">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle Navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a href="index.php" class="navbar-brand"><?echo $yourdomain;?></a>
+						</div>
+							<? include ('navigation.php'); ?>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="intro-mini">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>Premium Hosting</h1>
+					<h3>Powered By iFastNet</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	<div class="services">
+		<div class="container">
+		<h2>Our premium hosting features</h2>
+		<hr />
+		<table class="features-table">
+			<tr>
+				<td>Monthly Bandwidth</td>
+				<td align="center"><span>Up to Unlimited</span></td>
+			</tr>
+			<tr>
+				<td>Web Disk Space</td>
+				<td align="center"><span>Up to Unlimited</span></td>
+			</tr>
+			<tr>
+				<td>Price</td>
+				<td align="center"><span>Starting from $0.99/Month</span></td>
+			</tr>
+			<tr>
+				<td>Addon Domains</td>
+				<td align="center">Up to Unlimited</td>
+			</tr>
+			<tr>
+				<td>Parked Domains</td>
+				<td align="center">Up to Unlimited</td>
+			</tr>
+			<tr>
+				<td>Sub Domains</td>
+				<td align="center">Up to Unlimited</td>
+			</tr>
+			<tr>
+				<td>Email Accounts</td>
+				<td align="center">Up to Unlimited</td>
+			</tr>
+			<tr>
+				<td>MySQL Databases</td>
+				<td align="center">Up to Unlimited</td>
+			</tr>
+			<tr>
+				<td>FTP Accounts</td>
+				<td align="center">Up to Unlimited</td>
+			</tr>
+			<tr>
+				<td>Free Domain</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>SiteBuilder</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Site Statistics</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Softaculous Script Installer</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Custom Error Pages</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Cron Jobs</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>PHP Flags Manager</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>IP Address Deny</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>File Size Limit</td>
+				<td align="center">Unlimited</td>
+			</tr>
+			<tr>
+				<td>Custom CNAME Records</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Unmetered MySQL space</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td><h4>SCRIPTING FEATURES</h4></td>
+				<td align="center"></td>
+			</tr>
+			<tr>
+				<td>PHP</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>MySQL</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>PhpMyAdmin</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Ruby On Rails</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Custom CRON Jobs</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Full .htaccess Control</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td><h4>ECOMMERCE FEATURES</h4></td>
+				<td align="center"></td>
+			</tr>
+			<tr>
+				<td>PHP cart scripts install ability</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Password Protected Folders</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Bandwidth</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td><h4>SECURITY MECHANISM</h4></td>
+				<td align="center"></td>
+			</tr>
+			<tr>
+				<td>24/7 Monitoring</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Firewall Protection</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>UPS Power Back-up/Back-up</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Spam Filter Spam Assassin</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>			
+			<tr>
+				<td>File Backup & Restore</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Generator</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>			
+			<tr>
+				<td>Hotlink Protection</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td><h4>OUR TECHNOLOGY</h4></td>
+				<td align="center"></td>
+			</tr>
+			<tr>
+				<td>Cisco Powered Network</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Linux Clustered Server Network</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Intel Processors</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Apache Web Servers</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Cloud Servers</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>	
+			<tr>
+				<td><h4>OUR GUARANTEES</h4></td>
+				<td align="center"></td>
+			</tr>
+			<tr>
+				<td>5 Day Money Back</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>Price Freeze for the next year</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>99.9% Uptime Guarantee</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>No Hidden Charges</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>
+			<tr>
+				<td>24/ 7 Technical Support</td>
+				<td align="center"><div class="check"><img src="img/check.png"></div></td>
+			</tr>		
+		</table>
+		<a href="https://ifastnet.com/portal/sharedhosting.php" class="btnsign">Sign up now!</a>
+		</div>
+	</div>
+	<div class="features2">
+		<div class="container">	
+			<div class="row">
+				<div class="features2-col col-md-4">
+					<img src="img/hf1.jpg" alt="">
+					<h3>Free website builder</h3>
+					<p>An easy-to-use drag & drop free website builder tool that will help you create professional looking websites!</p>
+				</div>
+				<div class="features2-col col-md-4">
+					<img src="img/hf2.jpg" alt="">
+					<h3>Softaculous script installer!</h3>
+					<p>From Softaculous you'll get free access to over 250 free applications with an automatic update system!</p>
+				</div>
+				<div class="features2-col col-md-4">
+					<img src="img/hf3.jpg" alt="">
+					<h3>Free SEO Tools</h3>
+					<p>We will also offering free SEO tools to submit and help your websites get better ranking in the social networks!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="latest-col col-md-4">
+					<h3>Our Services</h3>
+					<ul>
+						<li class="clearfix">
+							<a href="free-hosting.php">Free Hosting</a>
+						</li>
+						<li class="clearfix">
+							<a href="premium-hosting.php">Premium Hosting</a>
+						</li>
+						<li  class="clearfix">
+							<a href="https://ifastnet.com/portal/vpshosting.php">VPS Hosting</a>
+						</li>
+						<li class="clearfix">
+							<a href="https://ifastnet.com/portal/dedicatedserver.php">Dedicated Servers</a>
+						</li>
+						<li  class="clearfix">
+							<a href="domains.php">Domain Name Registration</a>
+						</li>
+					</ul>
+				</div>
+				<div class="latest-col col-md-8">
+					<h3>Why us</h3>
+					<ul>
+    					<p>We use a powerful cluster of web servers that are all interconnected to act as one giant super computer. This technology is years ahead of most other hosting companies. Combining the power of many servers creates lightning fast website speed. Not only is the service extremely fast, it is resistant to failures that effect 'single server' hosting, used by most other free and paid hosting providers. If one of our clustered servers were to fail or have a problem, your website will continue to run normally using the working servers!</p>    
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<div class="copy">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<a>Terms of service</a> | <a>Privacy policy</a> | © HostName, Powered By <a href="https://ifastnet.com">iFastNet</a>.
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script> 
+	<script src="js/bootstrap.min.js"></script>
+</body>
+<!-- Template designed by iFastNet (iFastNet.com) exclusively for MyOwnFreeHost.com users -->
+</html>
