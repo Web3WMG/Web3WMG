79d27f2496361cb13c261bca4e3ac4c0d9068df5
The Homogeneral <146903198+homogeneral@users.noreply.github.com>
2023-12-05 17:59:56 -0600

    3WMG

---
+++ navigation.php
@@ -0,0 +1,11 @@
+<div class="navbar-collapse collapse">
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="free-hosting.php">Free Hosting</a></li>
+        <li><a href="premium-hosting.php">Premium Hosting</a></li>
+        <li><a href="domains.php">Domain Names</a></li>
+        <li><a href="whyus.php">Why Us</a></li>
+        <li><a href="support.php">Support</a></li>
+        <li><a href="http://cpanel.<? echo "$yourdomain" ;?>">Login</a></li>
+    </ul>
+</div>
